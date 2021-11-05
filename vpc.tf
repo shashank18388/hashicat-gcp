d85df3cb6d6c9a77618d2f94f8f7a4d3109ed997
shashank <shashank.183@gmail.com>
2021-11-05 13:13:10 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/ShashankTest/network/google"
+  version = "3.4.0"
+  # insert required variables here
+network_name = "gaurav_network"
+project_id = "var.project"
+subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
